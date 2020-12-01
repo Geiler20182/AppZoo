23aad29055fb98ea91940e582917cdf4a34d3ddf
Geiler20182 <geiler20182@javerianacali.edu.co>
2020-12-01 15:34:50 -0500

    mapa

--- src/components/Mapa/Mapa.js
+++ src/components/Mapa/Mapa.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './Mapa.css';
-
+import mapa_zoo from './sub_mapas/mapa_principal.jpg'
 class Mapa extends Component {
     render() {
         return(
-            <div>
+            <div className="mapa_zoo">
                 <div>
                     <h1>Mapa</h1>
+                    <img src = {mapa_zoo} />
                 </div>
             </div>
         );
--- zoo_server-develop/README.md
+++
@@ -1,2 +0,0 @@
-# Zoo Server
-Servidor para el proyecto del curso Diseño de Interfaces, **Zoo App**
